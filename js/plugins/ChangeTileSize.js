b2d1e63cc067956742bd160a89cf28744bfc6a6f
Mac15001900 <mac15001900@gmail.com>
2023-10-13 13:39:59 +0200

    Formatting

--- js/plugins/ChangeTileSize.js
+++ js/plugins/ChangeTileSize.js
@@ -47,167 +47,167 @@ RESOURCE LIST - DO NOT MODIFY BELOW THIS LINE
 RESOURCE LIST - DO NOT MODIFY ABOVE THIS LINE
  */
 
-(function() {
+(function () {
 
-  var parameters = $plugins.filter(function(p) { return p.description.contains('<ChangeTileSize>'); })[0].parameters;
-  var tileSize = parseInt(parameters['Tile Size'] || 48);
-  var tilesetsFolder = String(parameters['Tileset Image Folder'] || 'img/tilesets/');
-  var parallaxesFolder = String(parameters['Parallax Image Folder'] || 'img/parallaxes/');
+    var parameters = $plugins.filter(function (p) { return p.description.contains('<ChangeTileSize>'); })[0].parameters;
+    var tileSize = parseInt(parameters['Tile Size'] || 48);
+    var tilesetsFolder = String(parameters['Tileset Image Folder'] || 'img/tilesets/');
+    var parallaxesFolder = String(parameters['Parallax Image Folder'] || 'img/parallaxes/');
 
-  ImageManager.loadTileset = function(filename, hue) {
-    return this.loadBitmap(tilesetsFolder, filename, hue, false);
-  };
+    ImageManager.loadTileset = function (filename, hue) {
+        return this.loadBitmap(tilesetsFolder, filename, hue, false);
+    };
 
-  ImageManager.loadParallax = function(filename, hue) {
-      return this.loadBitmap(parallaxesFolder, filename, hue, true);
-  };
+    ImageManager.loadParallax = function (filename, hue) { //Change: loading some maps dynamically depending on language
+        return this.loadBitmap(parallaxesFolder, filename, hue, true);
+    };
 
-  Game_Map.prototype.tileWidth = function() {
-    return tileSize;
-  };
+    Game_Map.prototype.tileWidth = function () {
+        return tileSize;
+    };
 
-  Game_Map.prototype.tileHeight = function() {
-    return tileSize;
-  };
+    Game_Map.prototype.tileHeight = function () {
+        return tileSize;
+    };
 
-  Game_Vehicle.prototype.maxAltitude = function() {
-    return tileSize;
-  };
+    Game_Vehicle.prototype.maxAltitude = function () {
+        return tileSize;
+    };
 
-  var Tilemap_initialize = Tilemap.prototype.initialize;
-  Tilemap.prototype.initialize = function() {
-    Tilemap_initialize.call(this);
-    this._tileWidth = tileSize;
-    this._tileHeight = tileSize;
-  };
+    var Tilemap_initialize = Tilemap.prototype.initialize;
+    Tilemap.prototype.initialize = function () {
+        Tilemap_initialize.call(this);
+        this._tileWidth = tileSize;
+        this._tileHeight = tileSize;
+    };
 
-  // Add image files to required assets list, to ensure they are included
-  // in a deployed project
-  var _changeTileSize_Scene_Boot_start = Scene_Boot.prototype.start;
-  Scene_Boot.prototype.start = function() {
-    _changeTileSize_Scene_Boot_start.call(this);
+    // Add image files to required assets list, to ensure they are included
+    // in a deployed project
+    var _changeTileSize_Scene_Boot_start = Scene_Boot.prototype.start;
+    Scene_Boot.prototype.start = function () {
+        _changeTileSize_Scene_Boot_start.call(this);
 
-    if (!Utils.isOptionValid('test') || DataManager.isBattleTest() ||
-      DataManager.isEventTest()) {
-        return;
-      }
+        if (!Utils.isOptionValid('test') || DataManager.isBattleTest() ||
+            DataManager.isEventTest()) {
+            return;
+        }
 
-    // Read in this script
-    var fs = require('fs');
-    var path = window.location.pathname.replace(/(\/www|)\/[^\/]*$/, '/');
-    if (path.match(/^\/([A-Z]\:)/)) {
-      path = path.slice(1);
-    }
-    path = decodeURIComponent(path);
-    var scriptName = $plugins.filter(function(p) {
-      return p.description.contains('<ChangeTileSize>') && p.status;
-    })[0].name + '.js';
-    var sourcePath = path + 'js/plugins/' + scriptName;
-    var dataPath = path + 'data/';
-    if (fs.existsSync(sourcePath)) {
-      var source = fs.readFileSync(sourcePath, { encoding: 'utf8' });
-      source = source.split('\r\n');
-      var resListStart = source.indexOf('RESOURCE LIST - DO NOT MODIFY BELOW THIS LINE');
-      var resListEnd = source.indexOf('RESOURCE LIST - DO NOT MODIFY ABOVE THIS LINE');
-      if (resListStart === -1 || resListEnd === -1) {
-        console.log('ERROR - Unable to create resource list for ' + scriptName);
-        console.log('Please redownload the script and paste over your current version');
-      } else {
-        // remove previous list of resources
-        source.splice(resListStart + 1, resListEnd - resListStart - 1);
-        // build list of new resources
-        var resTilesets = [];
-        var resParallaxes = [];
+        // Read in this script
+        var fs = require('fs');
+        var path = window.location.pathname.replace(/(\/www|)\/[^\/]*$/, '/');
+        if (path.match(/^\/([A-Z]\:)/)) {
+            path = path.slice(1);
+        }
+        path = decodeURIComponent(path);
+        var scriptName = $plugins.filter(function (p) {
+            return p.description.contains('<ChangeTileSize>') && p.status;
+        })[0].name + '.js';
+        var sourcePath = path + 'js/plugins/' + scriptName;
+        var dataPath = path + 'data/';
+        if (fs.existsSync(sourcePath)) {
+            var source = fs.readFileSync(sourcePath, { encoding: 'utf8' });
+            source = source.split('\r\n');
+            var resListStart = source.indexOf('RESOURCE LIST - DO NOT MODIFY BELOW THIS LINE');
+            var resListEnd = source.indexOf('RESOURCE LIST - DO NOT MODIFY ABOVE THIS LINE');
+            if (resListStart === -1 || resListEnd === -1) {
+                console.log('ERROR - Unable to create resource list for ' + scriptName);
+                console.log('Please redownload the script and paste over your current version');
+            } else {
+                // remove previous list of resources
+                source.splice(resListStart + 1, resListEnd - resListStart - 1);
+                // build list of new resources
+                var resTilesets = [];
+                var resParallaxes = [];
 
-        // get tilesets & parallaxes that are actually used
-        $dataMapInfos.forEach(function(mapinfo) {
-          if (mapinfo) {
-            var map = JSON.parse(fs.readFileSync(dataPath + 'Map%1.json'.format(mapinfo.id.padZero(3))));
+                // get tilesets & parallaxes that are actually used
+                $dataMapInfos.forEach(function (mapinfo) {
+                    if (mapinfo) {
+                        var map = JSON.parse(fs.readFileSync(dataPath + 'Map%1.json'.format(mapinfo.id.padZero(3))));
 
-            if (!resTilesets.contains(map.tilesetId)) {
-              resTilesets.push(map.tilesetId);
-            }
+                        if (!resTilesets.contains(map.tilesetId)) {
+                            resTilesets.push(map.tilesetId);
+                        }
 
-            if (map.parallaxName !== '' && !resParallaxes.contains(map.parallaxName)) {
-              resParallaxes.push(map.parallaxName);
-            }
+                        if (map.parallaxName !== '' && !resParallaxes.contains(map.parallaxName)) {
+                            resParallaxes.push(map.parallaxName);
+                        }
 
-            // map events
-            map.events.forEach(function (event) {
-              if (event) {
-                event.pages.forEach(function (page) {
-                  if (page) {
-                    getResources(page.list, resTilesets, resParallaxes);
-                  }
+                        // map events
+                        map.events.forEach(function (event) {
+                            if (event) {
+                                event.pages.forEach(function (page) {
+                                    if (page) {
+                                        getResources(page.list, resTilesets, resParallaxes);
+                                    }
+                                })
+                            }
+                        })
+                    }
+                });
+
+                // common events
+                $dataCommonEvents.forEach(function (commonEvent) {
+                    if (commonEvent) {
+                        getResources(commonEvent.list, resTilesets, resParallaxes);
+                    }
+                });
+
+                // troop events
+                $dataTroops.forEach(function (troop) {
+                    if (troop) {
+                        troop.pages.forEach(function (page) {
+                            if (page) {
+                                getResources(page.list, resTilesets, resParallaxes);
+                            }
+                        })
+                    }
                 })
-              }
-            })
-          }
+
+                // add resource list to script
+                var inspos = resListStart + 1;
+
+                resTilesetNames = [];
+                resTilesets.forEach(function (tilesetId) {
+                    var tileset = $dataTilesets[tilesetId];
+                    if (tileset) {
+                        tileset.tilesetNames.forEach(function (tilesetName) {
+                            if (tilesetName !== '' && !resTilesetNames.contains(tilesetName)) {
+                                resTilesetNames.push(tilesetName);
+                            }
+                        })
+                    }
+                });
+
+                var tag = ' * @requiredAssets';
+                source.splice(inspos, 0, tag);
+
+                resTilesetNames.forEach(function (tilesetName) {
+                    tag = ' * @requiredAssets ' + tilesetsFolder + tilesetName;
+                    source.splice(inspos, 0, tag);
+                });
+
+                resParallaxes.forEach(function (parallaxName) {
+                    tag = ' * @requiredAssets ' + parallaxesFolder + parallaxName;
+                    source.splice(inspos, 0, tag);
+                });
+
+                // and save the file
+                source = source.join('\r\n');
+                fs.writeFileSync(sourcePath, source, { encoding: 'utf8' });
+            }
+        }
+    };
+
+    getResources = function (list, aryTilesets, aryParallaxes) {
+        list.filter(function (cmd) {
+            return cmd.code === 282 || cmd.code === 284;
+        }).forEach(function (cmd) {
+            if (cmd.code === 282 && !aryTilesets.contains(cmd.parameters[0])) {
+                aryTilesets.push(cmd.parameters[0]);
+            }
+            if (cmd.code === 284 && !aryParallaxes.contains(cmd.parameters[0])) {
+                aryParallaxes.push(cmd.parameters[0]);
+            }
         });
-
-        // common events
-        $dataCommonEvents.forEach(function(commonEvent) {
-          if (commonEvent) {
-            getResources(commonEvent.list, resTilesets, resParallaxes);
-          }
-        });
-
-        // troop events
-        $dataTroops.forEach(function (troop) {
-          if (troop) {
-            troop.pages.forEach(function (page) {
-              if (page) {
-                getResources(page.list, resTilesets, resParallaxes);
-              }
-            })
-          }
-        })
-
-        // add resource list to script
-        var inspos = resListStart + 1;
-
-        resTilesetNames = [];
-        resTilesets.forEach(function(tilesetId) {
-          var tileset = $dataTilesets[tilesetId];
-          if (tileset) {
-            tileset.tilesetNames.forEach(function(tilesetName) {
-              if (tilesetName !== '' && !resTilesetNames.contains(tilesetName)) {
-                resTilesetNames.push(tilesetName);
-              }
-            })
-          }
-        });
-
-        var tag = ' * @requiredAssets';
-        source.splice(inspos, 0, tag);
-
-        resTilesetNames.forEach(function(tilesetName) {
-          tag = ' * @requiredAssets ' + tilesetsFolder + tilesetName;
-          source.splice(inspos, 0, tag);
-        });
-
-        resParallaxes.forEach(function(parallaxName) {
-          tag = ' * @requiredAssets ' + parallaxesFolder + parallaxName;
-          source.splice(inspos, 0, tag);
-        });
-
-        // and save the file
-        source = source.join('\r\n');
-        fs.writeFileSync(sourcePath, source, { encoding: 'utf8' });
-      }
-    }
-  };
-
-  getResources = function(list, aryTilesets, aryParallaxes) {
-    list.filter(function (cmd) {
-      return cmd.code === 282 || cmd.code === 284;
-    }).forEach(function(cmd) {
-      if (cmd.code === 282 && !aryTilesets.contains(cmd.parameters[0])) {
-        aryTilesets.push(cmd.parameters[0]);
-      }
-      if (cmd.code === 284 && !aryParallaxes.contains(cmd.parameters[0])) {
-        aryParallaxes.push(cmd.parameters[0]);
-      }
-    });
-  };
+    };
 })();

c6a8a3c82b3530925d04595f1c32bc53d293cd66
Mac15001900 <mac15001900@gmail.com>
2024-07-10 14:11:08 +0200

    Stretch mode's default value now depends on resolution

--- js/plugins/SRD_FullscreenToggleOption.js
+++ js/plugins/SRD_FullscreenToggleOption.js
@@ -60,8 +60,16 @@
 	var position = String(parameters['Position']).toLowerCase();
 	var persist = String(parameters['Persist Default?']).trim().toLowerCase() === 'true';
 
+	defaultStretchValue = function () { //Checks if screen stretching should be enabled by default (depends on user's screen resolution)
+		const TARGET_WIDTH = 1920, TARGET_HEIGHT = 1080;
+		let w = screen.width, h = screen.height;
+		if (w < TARGET_WIDTH || h < TARGET_HEIGHT) return true; //1080p won't fit on this screen, so we must (regretably) stretch
+		return Number.isInteger(w / TARGET_WIDTH) && Number.isInteger(h / TARGET_HEIGHT); //Also stretch if we can safely do so, because the screen is a multiple of 1080p
+	}
+
+
 	ConfigManager.fullscreen = defaultValue;
-	ConfigManager.stretchMode = true;
+	ConfigManager.stretchMode = defaultStretchValue();
 	ConfigManager.markerMode = false; //True when toggling, false when holding
 
 	const PREFERRED_WINDOW_SIZES = [[1920, 1080], [1280, 720], [640, 360]];
@@ -136,7 +144,7 @@
 		g.fullScreen = fullScreenValue;
 
 		let stretchModeValue = config['stretchMode'];
-		if (stretchModeValue === undefined) stretchModeValue = true;
+		if (stretchModeValue === undefined) stretchModeValue = defaultStretchValue();
 		this.stretchMode = stretchModeValue;
 		g.stretchMode = stretchModeValue;
 

9e58a230c86b45f2a3cf0c7550a819665fc41c42
Mac15001900 <mac15001900@gmail.com>
2024-09-19 19:15:40 +0200

    Fixed a crash with walking over erased events

--- js/plugins/MAC_RunNearbyEvent.js
+++ js/plugins/MAC_RunNearbyEvent.js
@@ -1,6 +1,6 @@
 /*:
  * @author Mac15001900
- * @plugindesc v1.3.1 Allows events to run other events in various ways.
+ * @plugindesc v1.3.2 Allows events to run other events in various ways.
  * 
  * @param With an invalid target
  * @desc What should the plugin do when trying to run a non-existent event or page?
@@ -409,7 +409,7 @@ window.MAC_RunNearbyEvent = {}; //Global object for accesibility by scripts/othe
     $.shouldClearDestination = function () {
         let id = $.getInterpreter().eventId();
         let event = $gameMap.event(id);
-        if (!event) return false;
+        if (!event || !event.page()) return false;
         let list = event.list().filter(e => e.code !== 108 && e.code !== 408); //We're accessing the list from event() instead of directly from the interpreter to get a version that (hopefully) hasn't been modified by plugins
         if (list.length === 2 && list[0].code === 355 && list[0].parameters[0].substr(0, 14) === 'runNearbyEvent') return false; //It runs a nearby event (old script way)
         if (list.length === 2 && list[0].code === 356 && list[0].parameters[0].substr(0, 8).toLowerCase() === 'runevent') return false; //It runs a nearby event (plugin command)

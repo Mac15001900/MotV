e7e88d6e62f3f0037310e0523d81a3014d7f6ecf
Mac15001900 <mac15001900@gmail.com>
2024-09-23 17:10:28 +0200

    Fixed some options still rendering over ones below them when toggled with the 'ok' key

--- js/plugins/Window_Options.js
+++ js/plugins/Window_Options.js
@@ -187,6 +187,7 @@ Window_Options.prototype.processOk = function () {
     } else {
         this.changeValue(symbol, !value);
     }
+    if (index + 1 < this._list.length) this.redrawItem(index + 1); //Some characters, such a 'g', extend to the option below, so it must also be redrawn
 };
 
 Window_Options.prototype.cursorRight = function (wrap, reverse = false) {
